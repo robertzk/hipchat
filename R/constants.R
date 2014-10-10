6d891cec4c586cae85f12cd2acc3badec736d8b1
RobertZK <technoguyrob@gmail.com>
2014-10-10 18:03:48 -0500

    initial progress on determining the correct hipchat URL

--- R/constants.R
+++ R/constants.R
@@ -1,3 +1,3 @@
-hipchat_api_url <- 'https://api.hipchat.com/v2/'
+hipchat_api_url <- 'https://api.hipchat.com/v2'
 
 
--- R/send.R
+++ R/send.R
@@ -3,7 +3,8 @@
 #' @param type character. One of the native Hipchat API domains:
 #'    \code{c('addon', 'capabilities', 'emoticon', 'oauth', 'room' 'user')}.
 #' @param var character. Whatever goes after the \code{type} in the
-#'    hipchat API url, typically room ID, name, or user email.
+#'    hipchat API url, typically room ID, name, or user email,
+#'    depending on the API path you are calling.
 #' @param ... HTTP arguments to send to the Hipchat API. Unnamed arguments
 #'    will be passed in to the URL; named arguments will be assumed to be
 #'    API parameters. See the examples section.
@@ -33,6 +34,28 @@
 #'   # posts to https://api.hipchat.com/v2/room/My room/invite/your@@friend.org
 #' }
 hipchat_send <- function(type, var, ..., api_token = hipchat_api_token()) {
-  # TODO: (RK) Implement this.
+  url <- hipchat_url(type, var, ...)
+
+  # httr::POST(url, 
+  url
+}
+
+#' Hipchat API url.
+#'
+#' @param ... concatenate these arguments in to the hipchat URL.
+#' @param use.auth_token logical. Whether or not to provide an auth_token
+#'    GET query parameter, by default \code{TRUE}.
+#' @return https://api.hipchat.com/v2/...
+#' @examples
+#' stopifnot(hipchat_url('oauth', 'token', use.auth_token = FALSE) == 
+#'   paste(hipchat:::hipchat_api_url, 'oauth', 'token', sep = '/'))
+hipchat_url <- function(..., use.auth_token = TRUE) {
+  args <- list(...)
+  unnamed <- function(x) (names(x) %||% rep("", length(x))) == ''
+  url_elements <- unlist(args[unnamed(args)])
+  url <- do.call(paste, as.list(c(hipchat:::hipchat_api_url, url_elements, sep = '/')))
+  if (isTRUE(use.auth_token))
+    paste(url, paste('auth_token', hipchat_api_token(), sep = '='), sep = '?')
+  else url
 }
 
