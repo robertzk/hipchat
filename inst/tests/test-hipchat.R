251a597ca615874e0a8b0e89a8b937cb27f075d5
RobertZK <technoguyrob@gmail.com>
2014-10-11 23:52:48 -0500

    test that hipchat message sending fails for non-numeric / character inputs

--- inst/tests/test-hipchat.R
+++ inst/tests/test-hipchat.R
@@ -5,3 +5,9 @@ test_that('it errors when no room or message is given', {
   expect_error(hipchat(character(0), ''), 'Please specify')
   expect_error(hipchat('', c()), 'Please specify')
 })
+
+test_that('it errors when a non-character is passed', {
+  expect_error(hipchat(TRUE, TRUE))
+  expect_error(hipchat('a', TRUE))
+  expect_error(hipchat(TRUE, 'a'))
+})
