92e00d11f3482773b3d2f57431b45dfa454da43d
RobertZK <technoguyrob@gmail.com>
2014-10-11 19:57:25 -0500

    Introduce httr_with_json hack for httr::POST

--- R/send.R
+++ R/send.R
@@ -3,7 +3,7 @@
 #' @param type character. One of the native Hipchat API domains:
 #'    \code{c('addon', 'capabilities', 'emoticon', 'oauth', 'room' 'user')}.
 #' @param var character. Whatever goes after the \code{type} in the
-#'    hipchat API url, typically room ID, name, or user email,
+#'    hipchat API url, typically room ID, or user email,
 #'    depending on the API path you are calling.
 #' @param ... HTTP arguments to send to the Hipchat API. Unnamed arguments
 #'    will be passed in to the URL; named arguments will be assumed to be
@@ -34,28 +34,30 @@
 #'   # posts to https://api.hipchat.com/v2/room/My room/invite/your@@friend.org
 #' }
 hipchat_send <- function(type, var, ..., api_token = hipchat_api_token()) {
-  url <- hipchat_url(type, var, ...)
+  url <- if (missing(var)) hipchat_url(type, ...) else hipchat_url(type, var, ...)
+  named <- function(x) nzchar(names(x) %||% rep("", length(x)))
+  params <- list(...)
+  params <- params[named(params)]
 
-  # httr::POST(url, 
-  url
+  browser()
+  httr::POST(url, body = params)
 }
 
 #' Hipchat API url.
 #'
 #' @param ... concatenate these arguments in to the hipchat URL.
-#' @param use.auth_token logical. Whether or not to provide an auth_token
-#'    GET query parameter, by default \code{TRUE}.
+#' @inheritParams hipchat_send
 #' @return https://api.hipchat.com/v2/...
 #' @examples
 #' stopifnot(hipchat_url('oauth', 'token', use.auth_token = FALSE) == 
 #'   paste(hipchat:::hipchat_api_url, 'oauth', 'token', sep = '/'))
-hipchat_url <- function(..., use.auth_token = TRUE) {
+hipchat_url <- function(..., api_token = hipchat:::hipchat_api_token()) {
   args <- list(...)
-  unnamed <- function(x) (names(x) %||% rep("", length(x))) == ''
+  unnamed <- function(x) !nzchar(names(x) %||% rep("", length(x)))
   url_elements <- unlist(args[unnamed(args)])
   url <- do.call(paste, as.list(c(hipchat:::hipchat_api_url, url_elements, sep = '/')))
-  if (isTRUE(use.auth_token))
-    paste(url, paste('auth_token', hipchat_api_token(), sep = '='), sep = '?')
+  if (!is.null(api_token))
+    paste(url, paste('auth_token', api_token, sep = '='), sep = '?')
   else url
 }
 
--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,45 @@
 hipchat_api_token <- function()
   getOption('hipchat.api_token') %||% Sys.getenv('HIPCHAT_API_TOKEN')
 
+# A temporary workaround until hadley fixes httr
+# https://github.com/hadley/httr/issues/159
+httr_with_json <- function(expr) {
+  testthatsomemore::package_stub('jsonlite', 'toJSON', function(...) rjson::toJSON(...), eval.parent(expr))
+}
+
+# Stolen from my testthatsomemore package
+# https://github.com/robertzk/testthatsomemore/blob/master/R/stub.R#L51
+package_stub <- function(package_name, function_name, stubbed_value, expr) {
+  if (!is.element(package_name, installed.packages()[,1]))
+    stop(gettextf("Could not find package %s for stubbing %s",
+                  sQuote(package_name), dQuote(function_name)))
+  stopifnot(is.character(function_name))
+  if (!is.function(stubbed_value))
+    warning(gettextf("Stubbing %s::%s with a %s instead of a function",
+            package_name, function_name, sQuote(class(stubbed_value)[1])))
+
+  namespaces <-
+    list(as.environment(paste0('package:', package_name)),
+         getNamespace(package_name))
+  if (!exists(function_name, envir = namespaces[[1]], inherits = FALSE))
+    namespaces <- namespaces[-1]
+  if (!exists(function_name, envir = tail(namespaces,1)[[1]], inherits = FALSE))
+    stop(gettextf("Cannot stub %s::%s because it must exist in the package",
+         package_name, function_name))
+
+  lapply(namespaces, unlockBinding, sym = function_name)
+
+  # Clean up our stubbing on exit
+  previous_object <- get(function_name, envir = tail(namespaces,1)[[1]])
+  on.exit({
+    lapply(namespaces, function(ns) {
+      tryCatch(error = function(.) NULL, assign(function_name, previous_object, envir = ns))
+      lockBinding(function_name, ns)
+    })
+  })
+
+  lapply(namespaces, function(ns)
+    assign(function_name, stubbed_value, envir = ns))
+  eval.parent(substitute(expr))
+}
+
